092b25568521e259df507e20a5a761b78a9d61c2
Ash <aishasiddiqa151@gmail.com>
2024-07-23 14:22:09 -0700

    Create an array of questions for user input

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ import inquirer from 'inquirer';
 import generateMarkdown from './utils/generateMarkdown.js';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is the title of your project?",
+    "What is the description of your project?",
+    "What are the instructions for installation?",
+    "What is the usage of your project?",
+    "What is the license of your project?",
+    "Any contributers you would like to provide?",
+    "What are the tests of your project?",
+    "Questions?"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}

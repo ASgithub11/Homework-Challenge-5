f2f5b3d1b899dd2b5d490c19ed57837a493dcd9e
Ash <aishasiddiqa151@gmail.com>
2024-07-26 12:42:15 -0700

    Add function that returns license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,23 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const badgeApache = '![License: Apache](https://img.shields.io/badge/License-Apache-blue)';
+  const badgeMIT = '![License: MIT](https://img.shields.io/badge/License-MIT-blue)';
+  const badgeISC = '![License: ISC](https://img.shields.io/badge/License-ISC-blue)';
+  const badgeGPL = '![License: GPLv3](https://img.shields.io/badge/License-GPLv3-blue)';
+  
+  if (license === 'Apache') {  
+    return badgeApache;
+  } else if (license === 'MIT') {
+    return badgeMIT; 
+  } else if (license === 'ISC') {
+    return badgeISC;
+  } else if (license === 'GPLv3') {
+    return badgeGPL;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
